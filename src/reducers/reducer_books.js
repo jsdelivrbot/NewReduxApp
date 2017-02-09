7959d8cc56789d273ad5569f1ff5b3c92a641773
Omar Ceesay <Omar.Ceesay@bluecrossmn.com>
2017-02-09 09:57:04 -0600

    2nd

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    { title: 'Javascript: The Good Parts'},
+    { title: "Green Egges and Ham"},
+    { title: "Walden"},
+    { title: "The Things They Carry"}
+  ]
+}
